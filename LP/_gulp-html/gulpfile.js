208278a176f5144db2b37ea4a0a8038293dc8ee3
円山裕司 <>
2023-11-24 11:40:17 +0900

    gulp環境最新へ

--- LP/_gulp-html/gulpfile.js
+++ LP/_gulp-html/gulpfile.js
@@ -1,14 +1,12 @@
 const gulp = require('gulp');
 const bourbon = require('node-bourbon');
 const sass = require('gulp-sass')(require('sass'));
-const frontnote = require("gulp-frontnote");
 const uglify = require("gulp-uglify");
 const rename = require('gulp-rename');
 const browser = require("browser-sync");
 const plumber = require("gulp-plumber");
 const notify = require('gulp-notify');
 const csscomb = require('gulp-csscomb');
-const csslint = require('gulp-csslint');
 const jshint = require('gulp-jshint');
 const imagemin = require('gulp-imagemin');
 const mozjpeg = require("imagemin-mozjpeg");
@@ -35,7 +33,7 @@ const f_es5 = "es5/";
 const other = "other/";
 const f_code = "html/";
 const f_image_gulp = "img/";
-const root_font_px = 15;
+const root_font_px = 16;
 // const proxy = 'http://localhost/';
 const baseDir = '../';
 const browsers = [
@@ -54,142 +52,65 @@ const browsers = [
       done();
   });
 
-  bourbon.with( f_sass + '/style.scss');
+  bourbon.with(f_sass + '/style.scss');
+
   //sass
   gulp.task('sass', (done) => {
-    return gulp.src(f_sass + '**/*.scss')
-      .pipe(plumber({
-        errorHandler: notify.onError('<%= error.message %>')
-      }))
-      .pipe(sass({
-        includePaths: bourbon.includePaths
-      }))
-
-      .pipe(pleeease({
-        rem: false,
-        browsers: browsers,
-        minifier: false, //変更
-        mqpacker: true,
-      }))
-      .pipe(csscomb())
-      .pipe(gulp.dest(f_css))
-      //ブラウザリロード
-      .pipe(browser.reload({
-        stream: true
-      }));
-      done();
-  });
-
-  //cssComb実行
-  gulp.task('styles', (done) => {
-      return gulp.src([f_css + "**/*.css", "!" + f_css + "min/**/*.css"])
-      .pipe(csscomb())
-      .pipe(gulp.dest(f_css));
-      done();
-  });
-
-  //css構文チェック
-  gulp.task('csslint', (done) => {
-      gulp.src([f_css + "**/*.css", "!" + f_css + "min/**/*.css"])
-      .pipe(csslint())
-      .pipe(csslint.reporter());
-      done();
-  });
-
-  //スタイルガイド作成
-  gulp.task('guide', (done) => {
-      gulp.src(f_sass + '**/*.scss').pipe(frontnote({
-          css: '../css/style.css'
-      }));
-      done();
-  });
-
-  //HTML構文チェック
-  gulp.task('htmllint', (done) => {
-      gulp.src(f_code + '**/*.html')
-      .pipe(htmlhint())
-      .pipe(htmlhint.reporter());
-      done();
-  });
-
-  //JSのminify化
-  gulp.task("jsminify", (done) => {
-      gulp.src([f_js + "**/*.js", "!" + f_js + "min/**/*.js"])
-      .pipe(plumber({
-          errorHandler: notify.onError('<%= error.message %>')
-      }))
-      .pipe(uglify())
-      .pipe(gulp.dest(f_js + "min"))
-      //ブラウザリロード
-      .pipe(browser.reload({
-          stream: true
-      }));
-      done();
-  });
-  // JavaScript
-  // gulp.task('jsminify', (done) => {
-  //     return gulp
-  //         .src(f_js + "**/*.js")
-  //         .pipe(uglify()) //minify
-  //         .pipe(rename({
-  //             suffix: ".min",
-  //         }))
-  //         .pipe(gulp.dest(f_js));
-  //     done();
-  // })
-
-  //jsチェック
-  gulp.task('jslint', (done) => {
-      return gulp.src(f_js + '**/*.js')
-      .pipe(jshint())
-      .pipe(jshint.reporter('default'));
-      done();
-  });
+      return gulp.src(f_sass + '**/*.scss')
+          // watch中にエラーが発生してもwatchが止まらないようにする
+          .pipe(plumber({
+              errorHandler: notify.onError('<%= error.message %>')
+          }))
+          .pipe(sass({ //sassのコンパイル
+              includePaths: bourbon.includePaths
+          }))
+          .pipe(pleeease({
+              rem: false,
+              browsers: browsers,
+              minifier: true, //圧縮
+              mqpacker: true, //メディアクエリをまとめる
+              autoprefixer: false, //ベンダープレフィックスを付与 true | false
+              minifyOptions: {
+                  // 圧縮率を高める
+                  removeComments: true,
+                  collapseWhitespace: true,
+                  removeRedundantAttributes: true,
+                  minifySelectors: true,
+                  minifyUrls: true,
+              }
+          }))
+          // .pipe(csscomb()) //css整形する場合（上で圧縮しているので使用する場合注意）
+          .pipe(gulp.dest(f_css))
+          //ブラウザリロード
+          .pipe(browser.reload({
+              stream: true,
+          }));
+          done();
+      });
 
   //画像圧縮
   gulp.task("imagemin", function () {
-    return gulp
+      return gulp
       .src([f_image_gulp + '**/*.{png,jpg,gif,svg}'])
       .pipe(changed(f_image_min)) // gulp-changed必要
       .pipe(
-        imagemin([
-          pngquant({
-            quality: [.60, .70], // 画質
-            speed: 1 // スピード
-          }),
-          mozjpeg({ quality: 80 }), // 画質
-          imagemin.svgo(),
-          imagemin.optipng(),
-          imagemin.gifsicle({ optimizationLevel: 3 }) // 圧縮率
-        ])
+          imagemin([
+              pngquant({
+              quality: [.70, .85], // 画質
+              speed: 1 // スピード
+              }),
+              mozjpeg({ quality: 85 }), // 画質
+              imagemin.svgo(),
+              imagemin.optipng(),
+              imagemin.gifsicle({ optimizationLevel: 3 }) // 圧縮率
+          ])
       )
       .pipe(gulp.dest(f_image_min));
   });
 
-  //ベンダープレフィレクスとメディアクエリとコンブ
-  gulp.task('ple', (done) => {
-      return gulp.src([f_css + "**/*.css", "!" + f_css + "min/**/*.css"])
-          .pipe(plumber({
-              errorHandler: notify.onError('<%= error.message %>')
-          }))
-          .pipe(pleeease({
-              browsers: browsers,
-              minifier: false, //変更
-              mqpacker: true,
-              rem: [root_font_px + "px"]
-          }))
-          .pipe(csscomb())
-          .pipe(gulp.dest(f_css))
-          .pipe(browser.reload({
-              stream: true
-          }));
-          done();
-  });
-
-
   gulp.task('browserify', (done) => {
       browserify(f_es6 + 'browserify/main.js', {
-          debug: true //ソースマップの調整
+          debug: false //ソースマップの調整
       })
           .transform(babelify, {
               presets: ['es2015']
@@ -203,28 +124,32 @@ const browsers = [
         done();
   });
 
-
-
   gulp.task('babel', (done) => {
-    gulp.src([f_es6 + '**/*.js', "!" + f_es6 + 'browserify/*'])
-      .pipe(plumber({
-        errorHandler: notify.onError('<%= error.message %>')
-      }))
-      //  .pipe(sourcemaps.init())
-      .pipe(babel({
-        presets: ['es2015']
-      }))
-      // .pipe(sourcemaps.write('maps', {
-      //   includeContent: false,
-      //   sourceRoot: '/public/resources/maps'
-      // }))
-      .pipe(gulp.dest(f_js))
+      gulp.src([f_es6 + '**/*.js', "!" + f_es6 + 'browserify/*'])
+          .pipe(plumber({
+          errorHandler: notify.onError('<%= error.message %>')
+          }))
+          .pipe(babel({
+          presets: ['es2015']
+          }))
+          .pipe(gulp.dest(f_js)),
 
+      //jsファイルの圧縮
       gulp.src(f_es5 + '**/*.js')
-          .pipe(gulp.dest(f_js));
+          .pipe(plumber({
+              errorHandler: notify.onError('<%= error.message %>')
+              }))
+              .pipe(uglify()) //minify
+              .pipe(
+                  rename({
+                      suffix: '.min',
+                  })
+              )
+              .pipe(gulp.dest(f_js));
           done();
   });
 
+  //otherフォルダ内のファイルを書き出し
   gulp.task('move', (done) => {
       gulp.src(other + '**/*')
           .pipe(gulp.dest(f_html));
@@ -232,7 +157,7 @@ const browsers = [
   });
 
   gulp.task('code', (done) => {
-      gulp.src([f_code + "**/*.html"])
+      gulp.src([f_code + "**/*.php"])
           .pipe(browser.reload({
               stream: true
           }))
@@ -249,5 +174,3 @@ const browsers = [
       gulp.watch(f_image_gulp + '**', gulp.series('imagemin'));
       done();
   }));
-  gulp.task('comp', gulp.series(('styles', 'jsminify')));
-  gulp.task('check', gulp.series(('csslint', 'jslint')));
